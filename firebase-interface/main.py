2047dac39e42093553b63f1aa27fd97dab38466a
Erdem <erdemcanaz@gmail.com>
2023-05-02 01:52:37 +0300

    firebase uı is finished

--- firebase-interface/main.py
+++ firebase-interface/main.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime,time
 import firebase_admin
 from firebase_admin import credentials, db
 
@@ -15,7 +15,7 @@ import easygui
 # define function to update data
 def update_data(frame):
     # get data
-    for i, var_name in enumerate(var_names):
+    for i, var_name in enumerate(selected_vars):
         var_data[var_name].popleft()
         var_data[var_name].append(var_values[var_name])
         # clear axis
@@ -24,7 +24,13 @@ def update_data(frame):
         axs[i].plot(var_data[var_name])
         axs[i].scatter(len(var_data[var_name])-1, var_data[var_name][-1])
         axs[i].text(len(var_data[var_name])-1, var_data[var_name][-1]+2, "{}".format(var_data[var_name][-1]))
-        axs[i].set_ylim(0,100)
+        # set y-axis limits based on the min and max values of the data
+        var_min = np.min(var_data[var_name])
+        var_max = np.max(var_data[var_name])
+        var_range = var_max - var_min
+        ylim_min = var_min - var_range*0.1  # add 10% padding to the y-axis limits
+        ylim_max = var_max + var_range*0.1
+        axs[i].set_ylim(ylim_min, ylim_max)
         axs[i].set_title(var_name)
 
 # define variables to plot and start collections with zeros
@@ -34,10 +40,14 @@ var_values = {'BESS-voltage': 53.1, 'calculated-BESS-current': -0.13182674199623
 
 
 var_names = list(var_values.keys())
-var_data = {var_name: deque(np.zeros(100)) for var_name in var_names}
 
 # create a dialog box to select variables to plot
 selected_vars = easygui.multchoicebox(msg='Select variables to plot', title='Plot Variables', choices=var_names)
+
+# get number of data points as numeric input
+n_data_points_str = easygui.enterbox(msg='Enter number of data points', title='Data Points')
+n_data_points = int(n_data_points_str)
+var_data = {var_name: deque(np.zeros(n_data_points)) for var_name in var_names}
 
 # define and adjust figure
 fig, axs = plt.subplots(nrows=len(selected_vars), figsize=(12,6*len(selected_vars)), facecolor='#DEDEDE')
@@ -47,7 +57,8 @@ for ax in axs:
     ax.set_facecolor('#DEDEDE')
 
 # define animation
-ani = FuncAnimation(fig, update_data, interval=100)
+ani = FuncAnimation(fig, update_data, interval=1000)
+
 
 # start loop to continuously update the plot with new data
 while True:
@@ -59,21 +70,12 @@ while True:
             if key in selected_vars:
                 var_values[key] = value
 
-        # update the variables to plot based on the user's selection
-        selected_vars = easygui.multchoicebox(msg='Select variables to plot', title='Plot Variables', choices=var_names)
-
-        # update the figure based on the new selection
-        for ax in axs:
-            ax.clear()
-        axs = axs[:len(selected_vars)]
-        var_data = {var_name: deque(np.zeros(100)) for var_name in selected_vars}
-        for i, var_name in enumerate(selected_vars):
-            axs[i].set_title(var_name)
-            axs[i].set_facecolor('#DEDEDE')
-
         # start animation
-        ani.event_source.start()
-        plt.pause(5)
+        ani.event_source.start()      
+        plt.pause(1)
         ani.event_source.stop()
+        
+
+
     except KeyboardInterrupt:
         break
